9a3e9d24d90d4cac2d07461af09741b397364ec7
Glenaldin Halim <glenaldinhalim16@gmail.com>
2023-08-14 16:53:59 +0700

    update config, give initial value

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@
 import 'dotenv/config'
 
 const configuration = {
-    app_port: process.env.APP_PORT,
-    database_url: process.env.DATABASE_URL,
-    database_name: process.env.DATABASE_NAME
+    app_port: process.env.APP_PORT ? process.env.APP_PORT : 5000,
+    database_url: process.env.DATABASE_URL ? process.env.DATABASE_URL : 'tokoplay',
+    database_name: process.env.DATABASE_NAME ? process.env.DATABASE_NAME : 'mongodb://0.0.0.0:27017/'
 }
 
 export default configuration
